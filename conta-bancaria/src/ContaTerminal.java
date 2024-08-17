69d3f20b5884e3413375f413f62a1304a8c64d16
Leonardo Henrique <leonardohenriquesoarasalvez784@gmail.com>
2024-08-17 16:18:38 -0300

    feat: project dio

--- conta-bancaria/src/ContaTerminal.java
+++ conta-bancaria/src/ContaTerminal.java
@@ -1,5 +1,24 @@
+import java.math.BigDecimal;
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Por favor, digite o número");
+        int numero = scanner.nextInt();
+
+        System.out.println("Por favor, digite o número da Agência!");
+        String numeroAgencia = scanner.next();
+
+        System.out.println("Por favor, Insira seu primeiro nome!");
+        String primeiroNome = scanner.next();
+
+        System.out.println("Qual o saldo?");
+        BigDecimal saldo = scanner.nextBigDecimal();
+
+        System.out.println("Olá " + primeiroNome + ", obrigado por criar uma conta em nosso banco, sua agência é " + numeroAgencia + " e seu saldo " + saldo + " já está disponivél para saque.");
+
     }
 }
